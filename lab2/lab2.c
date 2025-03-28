765eaead23a7ef2505595700ae2218efe8fbb6ee
bardlii <bradleyjocelyn3@gmail.com>
2025-03-28 16:05:09 -0400

    Brad print debugging

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -152,7 +152,7 @@ int main()
         if (userTextInput[0] == '\n') { /* Enter key pressed */
           /* Send message to server and input it into buffer*/
           write(sockfd, userArrayInput, strlen(userArrayInput));
-          add_message(userArrayInput); /* Might not be necessary if it's handled in network thread?*/
+          // add_message(userArrayInput); /* Might not be necessary if it's handled in network thread?*/
           
           /* Reset cursor position */
           cursorHorizontalPosition = 0;
