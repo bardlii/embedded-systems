6560a96572207e83f32bfd555ca1f3cd191fe0f2
bardlii <bradleyjocelyn3@gmail.com>
2025-03-31 01:40:56 -0400

    long message text wrapping display debugging pt2

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -304,6 +304,10 @@ void add_message(const char message[2][MAX_MESSAGE_LENGTH]) {
     strcpy(message_buffer[i], message_buffer[i + 2]);
   }
 
+  /* Clear message buffer area to be filled */
+  memset(message_buffer[MAX_MESSAGES - 2], 0, MAX_MESSAGE_LENGTH);
+  memset(message_buffer[MAX_MESSAGES - 1], 0, MAX_MESSAGE_LENGTH);
+
   /* Add new messages to the buffer */
   strncpy(message_buffer[MAX_MESSAGES - 2], message[0], MAX_MESSAGE_LENGTH - 1);
   message_buffer[MAX_MESSAGES - 2][MAX_MESSAGE_LENGTH - 1] = '\0';
